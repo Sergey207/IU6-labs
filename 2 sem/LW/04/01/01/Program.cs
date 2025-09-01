ad5bef2cfdd2b6ca146b440d0f25d0fac6369665
Sergey <isergey207207@gmail.com>
2025-09-01 13:17:28 +0300

    2 sem LW4

---
+++ 2 sem/LW/04/01/01/Program.cs
@@ -0,0 +1,109 @@
+﻿// ReSharper disable SwapViaDeconstruction
+namespace _01;
+
+public static class Program
+{
+    private static int _n;
+
+    public static int Main()
+    {
+        Console.Write("Введите n (размер матрицы, кратно 3): ");
+        if (!int.TryParse(Console.ReadLine(), out _n))
+        {
+            Console.WriteLine("Ошибка при вводе числа!");
+            return 1;
+        }
+
+        if (_n < 3 || _n % 3 != 0)
+        {
+            Console.WriteLine("Число должно быть положительным и кратным 3!");
+            return 1;
+        }
+
+        var matrix = new int[_n][];
+        if (InputMatrix(matrix) != 0) return 1;
+        
+        PrintMiniMatrix(matrix);
+        ChangeMiniMatrix(matrix);
+        Console.WriteLine("\n");
+        PrintMiniMatrix(matrix);
+
+        return 0;
+    }
+
+    private static int InputMatrix(int[][] matrix)
+    {
+        Console.WriteLine($"Введите матрицу {_n}x{_n} целых чисел:");
+        for (var i = 0; i < _n; i++)
+        {
+            var row = Console.ReadLine();
+            if (row == null)
+            {
+                Console.WriteLine("Ошибка при вводе данных!");
+                return 1;
+            }
+
+            var elemRow = row.Trim().Split(" ");
+            if (elemRow.Length != _n)
+            {
+                Console.WriteLine($"Количество элементов должно быть равно {_n}!");
+                return 1;
+            }
+
+            var intRow = new int[_n];
+            for (var j = 0; j < _n; j++)
+            {
+                if (!int.TryParse(elemRow[j], out intRow[j]))
+                {
+                    Console.WriteLine("Ошибка при вводе данных!");
+                    return 1;
+                }
+            }
+
+            matrix[i] = intRow;
+        }
+
+        return 0;
+    }
+
+    private static void PrintMiniMatrix(int[][] matrix)
+    {
+        for (int i = 0; i < _n; i++)
+        {
+            for (int j = 0; j < _n; j++)
+            {
+                Console.Write(matrix[i][j] + " ");
+                if (j == (_n / 3 - 1) || j == (_n / 3 * 2 - 1))
+                    Console.Write(" ");
+            }
+
+            Console.WriteLine();
+            if (i == (_n / 3 - 1) || i == (_n / 3 * 2 - 1))
+                Console.WriteLine();
+        }
+    }
+
+    private static void ChangeMiniMatrix(int[][] matrix)
+    {
+        // Меняем матрицы на главной диагонали
+        for (var i = 0; i < _n / 3; i++)
+        {
+            for (var j = 0; j < _n / 3; j++)
+            {
+                var temp = matrix[i][j];
+                matrix[i][j] = matrix[i + _n * 2 / 3][j + _n * 2 / 3];
+                matrix[i + _n * 2 / 3][j + _n * 2 / 3] = temp;
+            }
+        }
+        // Меняем матрицы на побочной диагонали
+        for (var i = _n * 2 / 3; i < _n; i++)
+        {
+            for (var j = 0; j < _n / 3; j++)
+            {
+                var temp = matrix[i][j];
+                matrix[i][j] = matrix[i - _n * 2 / 3][j + _n * 2 / 3];
+                matrix[i - _n * 2 / 3][j + _n * 2 / 3] = temp;
+            }
+        }
+    }
+}
---
+++ 2 sem/LW/04/02/02/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace _02;
+
+public static class Program
+{
+    private static int _n;
+
+    private static int Main()
+    {
+        Console.Write("Введите число строк: ");
+        if (!int.TryParse(Console.ReadLine(), out _n))
+        {
+            Console.WriteLine("Введено неверное число строк!");
+            return 1;
+        }
+
+        if (_n <= 0)
+        {
+            Console.WriteLine("Число строк должно быть больше 0!");
+            return 1;
+        }
+
+        var strings = new string[_n];
+        if (InputStrings(strings) != 0)
+            return 1;
+
+        var result = new string[_n];
+        var nResult = 0;
+        for (var i = 0; i < _n; i++)
+        {
+            if ((i + 1) % 2 == 0)
+            {
+                if (strings[i][0] == 'c')
+                {
+                    result[nResult++] = strings[i];
+                    if (ChangeString(ref result[nResult - 1]) != 0) return 1;
+                }
+                else if (strings[i][0] != 'e')
+                    result[nResult++] = strings[i];
+            }
+            else
+            {
+                result[nResult++] = strings[i];
+            }
+        }
+
+        Console.WriteLine("Исходная последовательность:");
+        foreach (var s in strings)
+            Console.Write(s + " ");
+        Console.WriteLine("\nИзменённая последовательность:");
+        for (int i = 0; i < nResult; i++)
+            Console.Write(result[i] + " ");
+
+        return 0;
+    }
+
+    private static int InputStrings(string[] strings)
+    {
+        Console.WriteLine("Введите строки:");
+        for (var i = 0; i < _n; i++)
+        {
+            var input = Console.ReadLine();
+            if (input == null)
+            {
+                Console.WriteLine("Ошибка при вводе строки!");
+                return 1;
+            }
+
+            strings[i] = input;
+        }
+
+        return 0;
+    }
+
+    private static int ChangeString(ref string s)
+    {
+        Console.Write($"Введите строку, замещающую {s}: ");
+        var input = Console.ReadLine();
+        if (input == null)
+        {
+            Console.WriteLine("Ошибка при вводе строки!");
+            return 1;
+        }
+
+        s = input;
+        return 0;
+    }
+}
