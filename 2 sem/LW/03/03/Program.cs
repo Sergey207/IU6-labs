9d5784115043bdd6256ca8586284fd8cc9a3f05e
Sergey <isergey207207@gmail.com>
2025-09-01 13:11:30 +0300

    2 sem LW3

---
+++ 2 sem/LW/03/03/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _03;
+
+public class Program
+{
+    private static double a, b, c;
+
+    private static void Main()
+    {
+        Console.WriteLine("Эта программа решает уравнение Ax^2 + Bx + C = 0");
+        Console.WriteLine("Введите аргументы A, B, C: ");
+        if (!double.TryParse(Console.ReadLine(), out a) || !double.TryParse(Console.ReadLine(), out b) ||
+            !double.TryParse(Console.ReadLine(), out c))
+        {
+            Console.WriteLine("Ошибка в введённых данных!");
+            return;
+        }
+
+        Console.WriteLine("{0}x^2 + {1}x + {2} = 0", a, b, c);
+
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                {
+                    Console.WriteLine("x - любое действительное число!");
+                }
+                else
+                {
+                    Console.WriteLine("Корней среди действительных чисел нет!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("x = {0}", -c / b);
+            }
+        }
+        else
+        {
+            var D = b * b - 4 * a * c;
+            if (D == 0)
+                Console.WriteLine("x = {0}", -b / (2 * a));
+            else if (D < 0)
+                Console.WriteLine("Корней среди действительных чисел нет!");
+            else
+            {
+                Console.WriteLine("x1 = {0}", (-b - double.Sqrt(D)) / (2 * a));
+                Console.WriteLine("x2 = {0}", (-b + double.Sqrt(D)) / (2 * a));
+            }
+        }
+    }
+}
